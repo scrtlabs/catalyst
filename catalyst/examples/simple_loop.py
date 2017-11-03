2bbc0c00ccd9281914c33082e9549e7453e08e0a
fredfortier <fredfortier@gmail.com>
2017-11-02 20:44:16 -0400

    BLD: updating test algo

--- catalyst/examples/simple_loop.py
+++ catalyst/examples/simple_loop.py
@@ -21,7 +21,7 @@ def handle_data(context, data):
             context.asset,
             fields='price',
             bar_count=16,
-            frequency='1D'
+            frequency='5T'
         )
         rsi = talib.RSI(prices.values, timeperiod=14)[-1]
         print('got rsi: {}'.format(rsi))
@@ -29,25 +29,25 @@ def handle_data(context, data):
         print(e)
 
 
-run_algorithm(
-    capital_base=250,
-    start=pd.to_datetime('2017-1-1', utc=True),
-    end=pd.to_datetime('2017-10-22', utc=True),
-    data_frequency='minute',
-    initialize=initialize,
-    handle_data=handle_data,
-    analyze=None,
-    exchange_name='bitfinex',
-    algo_namespace='simple_loop',
-    base_currency='btc'
-)
 # run_algorithm(
+#     capital_base=250,
+#     start=pd.to_datetime('2017-1-1', utc=True),
+#     end=pd.to_datetime('2017-10-22', utc=True),
+#     data_frequency='daily',
 #     initialize=initialize,
 #     handle_data=handle_data,
 #     analyze=None,
-#     exchange_name='bittrex',
-#     live=True,
+#     exchange_name='bitfinex',
 #     algo_namespace='simple_loop',
-#     base_currency='eth',
-#     live_graph=False
+#     base_currency='btc'
 # )
+run_algorithm(
+    initialize=initialize,
+    handle_data=handle_data,
+    analyze=None,
+    exchange_name='poloniex',
+    live=True,
+    algo_namespace='simple_loop',
+    base_currency='eth',
+    live_graph=False
+)
