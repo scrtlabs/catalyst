c260e188b0c220f79173892299ffb0f63145ec1f
fredfortier <fredfortier@gmail.com>
2017-11-16 18:14:24 -0500

    BUG: looking a potential resampling issue

--- tests/exchange/test_utils.py
+++ tests/exchange/test_utils.py
@@ -4,11 +4,20 @@ from random import randint
 import pandas as pd
 
 
-def rnd_history_date_days(max_days=30):
-    now = pd.Timestamp.utcnow()
+def rnd_history_date_days(max_days=30, last_dt=None):
+    if last_dt is None:
+        last_dt = pd.Timestamp.utcnow()
+
     days = randint(0, max_days)
 
-    return now - timedelta(days=days)
+    return last_dt - timedelta(days=days)
+
+
+def rnd_history_date_minutes(max_minutes=1440):
+    now = pd.Timestamp.utcnow()
+    days = randint(0, max_minutes)
+
+    return now - timedelta(minutes=days)
 
 
 def rnd_bar_count(max_bars=21):
