12d5915c8ee3503770c387b0b6d623e53aef4915
Victor Grau Serrat <lacabra@gmail.com>
2017-11-27 15:02:13 -0700

    ENH: DEBUG level can be easily overriden from the local environment

--- catalyst/constants.py
+++ catalyst/constants.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
 import logbook
 
-LOG_LEVEL = logbook.DEBUG
+''' You can override the LOG level from your environment.
+    For example, if you want to see the DEBUG messages, run:
+    $ export CATALYST_LOG_LEVEL=10
+'''
+LOG_LEVEL = int(os.environ.get('CATALYST_LOG_LEVEL', logbook.INFO))
 
 DATE_TIME_FORMAT = '%Y-%m-%d %H:%M'
 
