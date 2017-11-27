c2f3e00d99730d09ed9ee03a695030984ba6c608
fredfortier <fredfortier@gmail.com>
2017-11-27 17:27:43 -0500

    BUG: Adding back missing constants

--- catalyst/constants.py
+++ catalyst/constants.py
@@ -9,6 +9,10 @@ import logbook
 '''
 LOG_LEVEL = int(os.environ.get('CATALYST_LOG_LEVEL', logbook.INFO))
 
+SYMBOLS_URL = 'https://s3.amazonaws.com/enigmaco/catalyst-exchanges/' \
+              '{exchange}/symbols.json'
+
 DATE_TIME_FORMAT = '%Y-%m-%d %H:%M'
+DATE_FORMAT = '%Y-%m-%d'
 
 AUTO_INGEST = False
