scrtlabs/catalyst

commit ce085e01ecb92375dd14ce552644280f2792f738

MAINT: PEP8 compliance

catalyst/__main__.py
@@ -10,7 +10,6 @@ from six import text_type
 from catalyst.data import bundles as bundles_module
 from catalyst.exchange.exchange_bundle import ExchangeBundle
 from catalyst.exchange.exchange_utils import delete_algo_folder
-from catalyst.exchange.factory import get_exchange
 from catalyst.utils.cli import Date, Timestamp
 from catalyst.utils.run_algo import _run, load_extensions
 
@@ -520,7 +519,7 @@ def live(ctx,
     default=False,
     help='Report potential anomalies found in data bundles.'
 )
-def ingest_exchange(exchange_name, data_frequency, start, end,
+def ingest_exchange(ctx, exchange_name, data_frequency, start, end,
                     include_symbols, exclude_symbols, csv, show_progress,
                     verbose, validate):
     """

commit ebd1ca44f623a1e68c702439a7e252593a4c7074

BUG: fix missing context in ingest-exchange, bug introduced in commit ce085e01ecb92375dd14ce552644280f2792f738

catalyst/__main__.py
@@ -521,6 +521,7 @@ def live(ctx,
     default=False,
     help='Report potential anomalies found in data bundles.'
 )
+@click.pass_context
 def ingest_exchange(ctx, exchange_name, data_frequency, start, end,
                     include_symbols, exclude_symbols, csv, show_progress,
                     verbose, validate):