ac2f0bbbf897e6f7340460bf0ba1658e09544db0
Frederic Fortier <fredfortier@gmail.com>
2017-12-28 17:51:59 -0500

    BLD: Removing old exchange implementations

--- catalyst/exchange/bittrex/extensions-example.py
+++
@@ -1,7 +0,0 @@
-from catalyst.data.bundles import register
-from catalyst.exchange.exchange_bundle import exchange_bundle
-
-symbols = (
-    'neo_btc',
-)
-register('exchange_bitfinex', exchange_bundle('bitfinex', symbols))
