627c30a740eb7f26ecf5c03d45adcb1450242c0d
AvishaiW <avishai@enigma.co>
2018-04-15 15:37:58 +0300

    BUG: lowered limit to include all coin values #296

--- catalyst/utils/math_utils.py
+++ catalyst/utils/math_utils.py
@@ -22,7 +22,7 @@ def round_nearest(x, a):
     return round(round(x / a) * a, -int(math.floor(math.log10(a))))
 
 
-def tolerant_equals(a, b, atol=10e-7, rtol=10e-7, equal_nan=False):
+def tolerant_equals(a, b, atol=10e-9, rtol=10e-9, equal_nan=False):
     """Check if a and b are equal with some tolerance.
 
     Parameters
