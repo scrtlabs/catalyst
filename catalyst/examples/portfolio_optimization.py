0dcd0fe3155fb5c400dca904b7793d7b46142a6a
Victor <lacabra@users.noreply.github.com>
2018-04-27 12:04:51 -0600

    DOC: authorship attribution to portf.optimization

--- catalyst/examples/portfolio_optimization.py
+++ catalyst/examples/portfolio_optimization.py
@@ -1,4 +1,8 @@
-'''Use this code to execute a portfolio optimization model. This code
+'''
+   Author: Rodrigo Gomez-Grassi
+   Date: Sep. 20, 2017  
+
+   Use this code to execute a portfolio optimization model. This code
    will select the portfolio with the maximum Sharpe Ratio. The parameters
    are set to use 180 days of historical data and rebalance every 30 days.
 
