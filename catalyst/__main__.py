ae1effa9067f3a46aaf17115d2cf151407729fa0
EmbarAlmog <embar@enigma.co>
2018-06-05 23:31:11 +0300

    ENH: Raise error when trying to ingest data of exchange that is not supported - issue #341

--- catalyst/__main__.py
+++ catalyst/__main__.py
@@ -14,8 +14,7 @@ from catalyst.exchange.exchange_bundle import ExchangeBundle
 from catalyst.exchange.utils.exchange_utils import delete_algo_folder
 from catalyst.utils.cli import Date, Timestamp
 from catalyst.utils.run_algo import _run, load_extensions
-
-from catalyst.constants import SUPPORTED_EXCHANGES
+from catalyst.exchange.utils.bundle_utils import EXCHANGE_NAMES
 
 try:
     __IPYTHON__
@@ -587,9 +586,12 @@ def ingest_exchange(ctx, exchange_name, data_frequency, start, end,
 
     if exchange_name is None:
         ctx.fail("must specify an exchange name '-x'")
-    if exchange_name not in SUPPORTED_EXCHANGES:
-        ctx.fail("ingest-exchange does not support {}, please choose exchange from: {}".format(exchange_name,
-                                                                                               SUPPORTED_EXCHANGES))
+    if exchange_name not in EXCHANGE_NAMES:
+        ctx.fail(
+            "ingest-exchange does not support {}, "
+            "please choose exchange from: {}".format(
+                exchange_name,
+                EXCHANGE_NAMES))
 
     exchange_bundle = ExchangeBundle(exchange_name)
 
